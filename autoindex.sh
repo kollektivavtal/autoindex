cdcf3a4a25a414179f6b83217194e10cb15ce9f7
Henry Catalini Smith <henry@catalinismith.com>
2025-01-01 22:30:25 +0100

    Try to fix spaces handling

--- autoindex.sh
+++ autoindex.sh
@@ -6,17 +6,14 @@ BASE_URL="$1"
 README_FILE="README.md"
 
 if [ ! -f "$README_FILE" ]; then
-  echo "# PDF Links" > "$README_FILE"
+  echo "# Kollektivavtal" > "$README_FILE"
+  echo "" >> "$README_FILE"
 fi
 
-if ! grep -q "^# Kollektivavtal" "$README_FILE"; then
-  echo -e "# Kollektivavtal\n$(cat $README_FILE)" > "$README_FILE"
-fi
-
-for pdf in $(find . -name "*.pdf"); do
+while IFS= read -r -d '' pdf; do
   pdf_name=$(basename "$pdf")
   link="* ${BASE_URL}${pdf_name}"
-  if ! grep -q "$link" "$README_FILE"; then
+  if ! grep -qF "$link" "$README_FILE"; then
     echo "$link" >> "$README_FILE"
   fi
-done
+done < <(find . -name "*.pdf" -print0)
