02a8acad38001e779f3b6588b2fc93c09148cbd8
Henry Catalini Smith <henry@catalinismith.se>
2025-01-02 09:07:31 +0100

    Swap to gh pages

--- autoindex.sh
+++ autoindex.sh
@@ -2,15 +2,30 @@
 
 set -e
 
-README_FILE="README.md"
+index_html="index.html"
 
-echo "# Kollektivavtal ${YEAR}" > "$README_FILE"
-echo "" >> "$README_FILE"
+echo "<!doctype html>" >> "$index_html"
+echo '<html lang="en" >' >> "$index_html"
+echo ' <head>' >> "$index_html"
+echo '  <meta charset="utf-8">' >> "$index_html"
+echo '  <title>Kollektivavtal</title>' >> "$index_html"
+echo ' </head>' >> "$index_html"
+echo ' <body>' >> "$index_html"
+echo '  <h1>Kollektivavtal</h1>' >> "$index_html"
+echo '  <ul>' >> "$index_html"
 
 while IFS= read -r -d '' pdf; do
   pdf_name=$(basename "$pdf")
   agreement_name="${pdf_name%.pdf}"
   encoded_pdf_name=$(echo "$pdf_name" | sed 's/ /%20/g')
-  link="* [${agreement_name}](http://kollektivavtal.github.io/${YEAR}/${encoded_pdf_name})"
-  echo "$link" >> "$README_FILE"
-done < <(find . -name "*.pdf" -print0)
+  echo '   <li>' >> "$index_html"
+  echo "    <a href=\"http://kollektivavtal.github.io/${YEAR}/${encoded_pdf_name}\">" >> "$index_html"
+  echo "     ${agreement_name}" >> "$index_html"
+  echo '    </a>' >> "$index_html"
+  echo '   </li>' >> "$index_html"
+  echo 
+done < <(find . -name "*.pdf" -print0 | sort)
+
+echo '  </ul>' >> "$index_html"
+echo ' </body>' >> "$index_html"
+echo '</html>' >> "$index_html"
