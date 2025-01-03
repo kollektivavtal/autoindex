a2693020d30bd2da785d70071bf2c70a83ff1e39
Henry Catalini Smith <henry@catalinismith.com>
2025-01-03 14:52:45 +0100

    Set up Tailwind with side-by-side layout:

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.njk", "./agreements.njk", "./_includes/*.njk"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
