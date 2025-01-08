e8843e6c828f5cfe7e810cff99684a907786e6f0
Henry Catalini Smith <henry@catalinismith.com>
2025-01-08 22:08:05 +0100

    Add language tag support

--- eleventy.test.ts
+++ eleventy.test.ts
@@ -10,21 +10,22 @@ describe("parseFilename", () => {
     });
   });
 
-  it("allows em dash as a separator", () => {
-    const result = parseFilename("Testavtalet – Bilaga.pdf");
-    expect(result).toEqual({
-      agreementName: "Testavtalet",
-      documentName: "Bilaga",
-      documentRank: 0,
-    });
-  });
-
   it("allows document rank as a separator", () => {
     const result = parseFilename("Testavtalet (2) Bilaga.pdf");
     expect(result).toEqual({
       agreementName: "Testavtalet",
       documentName: "Bilaga",
       documentRank: 2,
+    });
+  });
+
+  it("detects language at the end of the basename", () => {
+    const result = parseFilename("Testavtalet (2) Bilaga (en).pdf");
+    expect(result).toEqual({
+      agreementName: "Testavtalet",
+      documentName: "Bilaga",
+      documentRank: 2,
+      documentLanguage: "en",
     });
   });
 });
