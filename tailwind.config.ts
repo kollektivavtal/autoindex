bf4ddb29ed1f2636d15f6172135b056d52b1ae6f
Henry Catalini Smith <henry@catalinismith.se>
2025-01-24 20:40:16 +0100

    Clean slate

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: ["./index.njk", "./agreements.njk", "./_includes/*.njk"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
