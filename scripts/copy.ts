f85fe3ea22d46be22709bddd84d75aa32f1315ba
Henry Catalini Smith <henry@catalinismith.se>
2025-01-24 21:47:37 +0100

    Add copy script

---
+++ scripts/copy.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+
+if (process.argv.length < 3) {
+  console.log("Usage: pnpm copy <src> <dst>");
+  process.exit(1);
+}
+
+(async function () {
+  const [src, dst] = process.argv.slice(2).map((p) => path.resolve(p));
+
+  const pdfs = fs
+    .readdirSync(src)
+    .filter((p) => p.endsWith(".pdf"))
+    .map((p) => path.join(src, p));
+
+  for (const pdf of pdfs) {
+    const basename = path.basename(pdf);
+    const dstPath = path.join(dst, basename);
+    fs.copyFileSync(pdf, dstPath);
+    console.log(`Copied ${basename} to ${dst}`);
+  }
+})();
